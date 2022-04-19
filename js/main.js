76ad4d910295d6ff885243b65f57163b593134dc
2333263 <2333263@students.wits.ac.za>
2022-04-19 14:09:37 +0200

    added tracking of mouse when holding down left click

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ import * as THREE from '../node_modules/three/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+var mouse=new THREE.Vector2(0,0);
+var mouseDown=false;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -26,6 +27,27 @@ scene.add(light);
 scene.add(floor.translateZ(-6).translateY(-2));
 
 camera.position.z = 5;
+
+document.addEventListener("mousedown",onMouseDown);
+document.addEventListener("mouseup",onMouseUp);
+document.addEventListener("mousemove",onMouseMove);
+
+function onMouseDown(event){
+	mouseDown=true;
+	
+}
+function onMouseUp(event){
+	mouseDown=false;
+}
+function onMouseMove(event){
+	if(mouseDown){
+		mouse.x=(event.clientX/window.innerWidth);
+		mouse.y=-(event.clientY/window.innerHeight);
+		console.log(mouse.x+" "+ mouse.y);
+	}
+}
+
+
 
 function animate() {
 	requestAnimationFrame( animate );
