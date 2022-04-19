89fc095cca27757bef10af69a8500f446a171ce4
McDomdalds <benjaminlservant@gmail.com>
2022-04-19 16:46:40 +0200

    Added Loader

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	cube.rotation.z += 0.01;
-	controls.update(1.0);
+	//controls.update(1.0);
 	//floorMat.position.copy(groundBody.position);
 	//floorMat.quaternion.copy(groundBody.quaternion);
 	renderer.render( scene, camera );
