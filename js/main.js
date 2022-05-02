bed7798e0803ce3b7abe9ef856adcaeef500241a
2333263 <2333263@students.wits.ac.za>
2022-05-02 14:09:47 +0200

    Added working minimap
    Co-Authored-By: Justin 2356115 <101044525+J-HyperNova@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -4,16 +4,20 @@ import { PointerLockControls } from '/js/PointerLockControls.js';
 import {HUD} from "/js/HUD.js"
 import { Targets } from '/js/targets.js';
 
-const scene = new THREE.Scene();
+var scene = new THREE.Scene();
+var mapScene=new THREE.Scene();
 const aspectRatio= window.innerWidth / window.innerHeight
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
-var frustumSize = 10;	
-const pipcamera =  new THREE.OrthographicCamera(  2*frustumSize, 2*-frustumSize , frustumSize , -frustumSize , 0, 10 );
+var frustumSize = 14;	
+//2*frustumSize, 2*-frustumSize , frustumSize , -frustumSize , 0, 10 
+//(window.innerWidth-20)/(-2*frustumSize),(window.innerWidth-20)/(2*frustumSize),(window.innerHeight-20)/(2*frustumSize),(window.innerHeight-20)/(-2*frustumSize),1,1000 
+const pipcamera =  new THREE.OrthographicCamera(-frustumSize, frustumSize , frustumSize , -frustumSize , 1, 1000  );
 var Clock=new THREE.Clock(true)
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth-20, window.innerHeight-20);
 renderer.setClearColor(0xADD8E6,1)
 document.body.appendChild(renderer.domElement);
+
 const raycaster = new THREE.Raycaster();
 const loader = new THREE.TextureLoader();
 const timestep = 1 / 60
@@ -21,6 +25,7 @@ const material = new THREE.MeshStandardMaterial({
 	map: loader.load('textureOne.jpg') //test texture
 });
 const TargetArr=[];
+const mapTargetArr=[];
 const world = new CANNON.World({
 	gravity: new CANNON.Vec3(0, -22, 0) //Middle value is gravity in the y direction 
 });
@@ -49,22 +54,25 @@ camera.add(HudPlane.translateZ(-1));
 
 
 for(var i=0;i<5;i++){
-	var target=new Targets(i,0,2+2*i,0);
+	var target=new Targets(i,2+3*i,5,2*i);
 	TargetArr.push(target)
 	scene.add(target.getCylinder())
+	
+	
 }
 var target=new Targets(5,5,2,0);
 TargetArr.push(target)
 scene.add(target.getCylinder())
+
 hud.updateTargetNumbers(TargetArr.length,0)
 
 const geometry = new THREE.BoxGeometry(1,1,1);
 const floorGeo = new THREE.BoxGeometry(100, 0.1, 100);
-
 const floorMat = new THREE.MeshLambertMaterial({map: loader.load("goomba.png")}); //testure on floor to show depth of movement
 
 const light = new THREE.HemisphereLight("white", "white", 0.8);
 const floor = new THREE.Mesh(floorGeo, floorMat);
+
 const cube = new THREE.Mesh(geometry, material);
 
 const groundBody = new CANNON.Body({
@@ -76,20 +84,20 @@ const groundBody = new CANNON.Body({
 groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 0), Math.PI * 0.5); //flat surface 
 world.addBody(groundBody) //add floor to world
 
-scene.add(cube.translateZ(-6).translateY(2));
-scene.add(light);
-scene.add(floor.translateZ(-6).translateY(-2));
+
+scene.add(cube.translateZ(-6).translateY(2))
+scene.add(light)
+scene.add(floor.translateZ(-6).translateY(-2))
 camera.position.z = 9;
 camera.position.y = 9;
-pipcamera.position.set( 0,1, 0);
-pipcamera.rotateX(Math.PI/2)
-
+pipcamera.position.set( 0,30, 0);
+pipcamera.rotateX(-Math.PI/2)
 
 
 
 
 const player = new THREE.Mesh(new THREE.SphereGeometry(1.5), material);  //visibile representation of player hitbox
-scene.add(player);
+scene.add(player)
 const playerShape = new CANNON.Sphere(1.5);
 const playerBody = new CANNON.Body({ //player hitbox represented by sphere 
 	mass: 1,
@@ -137,7 +145,6 @@ world.addBody(playerBody);
 const controls = new PointerLockControls(camera, renderer.domElement);
 
 scene.add(controls.getObject());
-
 
 document.addEventListener('click', () => {
 	controls.lock();
@@ -220,14 +227,17 @@ function move() {
 	camera.position.copy(playerBody.position);
 	pipcamera.position.x=(playerBody.position.x);
 	pipcamera.position.z=(playerBody.position.z);
+	
 }
 
 floor.position.copy(groundBody.position);
 floor.quaternion.copy(groundBody.quaternion);
-
+console.log(scene)
+console.log(mapScene)
 function animate() {
 	world.step(timestep);
-
+	
+	
 	player.position.copy(playerBody.position);
 	player.quaternion.copy(playerBody.quaternion);
 	requestAnimationFrame(animate);
@@ -240,8 +250,35 @@ function animate() {
 	hud.updateAmmoCount(playerBody.noBullets,30)
 	hud.draw();
 	hudTexture.needsUpdate=true;
-
+	renderer.autoClear=false;
+	renderer.clear();
+	renderer.setViewport( 0, 0, window.innerWidth-20, window.innerHeight-20 );
+	renderer.render(scene, camera)
+    mapTargets();
+	renderer.clearDepth();
+	renderer.setViewport( window.innerWidth-250,  50, 200, 200 )
 	renderer.render(scene, pipcamera);
+	worldTargets();
+	
+	
 };
 
 animate();
+
+
+function mapTargets(){
+	for (var i=0;i<TargetArr.length;i++){
+		//scene.remove(TargetArr[i].getCylinder())
+		var tempCylinder=new THREE.Mesh(TargetArr[i].getCylinder().geometry,TargetArr[i].getCylinder().material)
+		tempCylinder.position.copy(TargetArr[i].getCylinder().position)
+		mapTargetArr.push(tempCylinder)
+		scene.add(tempCylinder.rotateY(Math.PI/2).translateY(5))
+	}
+
+	
+}
+function worldTargets(){
+	while(mapTargetArr.length!=0){
+		scene.remove(mapTargetArr.pop())
+	}
+}
