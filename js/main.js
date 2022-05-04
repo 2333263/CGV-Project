a45151fc4ae37623d1d57c945ddf615c3264f3c5
2333263 <2333263@students.wits.ac.za>
2022-05-04 13:02:22 +0200

    Added win and loss conditions

--- js/main.js
+++ js/main.js
@@ -1,78 +1,68 @@
 import * as THREE from 'three';
 import * as CANNON from 'cannon-es';
 import { PointerLockControls } from '/js/PointerLockControls.js';
-import {HUD} from "/js/HUD.js"
+import { HUD } from "/js/HUD.js"
 import { Targets } from '/js/targets.js';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import { threeToCannon, ShapeType } from 'three-to-cannon';
 
+
 var scene = new THREE.Scene();
-var mapScene=new THREE.Scene();
-const aspectRatio= window.innerWidth / window.innerHeight
+const aspectRatio = window.innerWidth / window.innerHeight
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
-const HudCamera=new THREE.OrthographicCamera(-window.innerWidth/2,window.innerWidth/2,window.innerHeight/2,-window.innerHeight/2,0,30)
-var sceneHUD=new THREE.Scene();
-var frustumSize = 14;	
+const HudCamera = new THREE.OrthographicCamera(-window.innerWidth / 2, window.innerWidth / 2, window.innerHeight / 2, -window.innerHeight / 2, 0, 30)
+var sceneHUD = new THREE.Scene();
+var frustumSize = 14;
 //2*frustumSize, 2*-frustumSize , frustumSize , -frustumSize , 0, 10 
 //(window.innerWidth-20)/(-2*frustumSize),(window.innerWidth-20)/(2*frustumSize),(window.innerHeight-20)/(2*frustumSize),(window.innerHeight-20)/(-2*frustumSize),1,1000 
-const pipcamera =  new THREE.OrthographicCamera(-frustumSize, frustumSize , frustumSize , -frustumSize , 1, 1000  );
-var Clock=new THREE.Clock(true)
+const pipcamera = new THREE.OrthographicCamera(-frustumSize, frustumSize, frustumSize, -frustumSize, 1, 1000);
+var Clock = new THREE.Clock(true)
 const renderer = new THREE.WebGLRenderer();
-renderer.antialias=true
-renderer.shadowMap.enabled=true;
-renderer.shadowMap.type=THREE.PCFSoftShadowMap;
-renderer.setSize(window.innerWidth-20, window.innerHeight-20);
-renderer.setClearColor(0xADD8E6,1)
+renderer.antialias = true
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
+renderer.setClearColor(0xADD8E6, 1)
 document.body.appendChild(renderer.domElement);
-
+const initposition=new CANNON.Vec3(0, 5, 4)
 const raycaster = new THREE.Raycaster();
 const loader = new THREE.TextureLoader();
 const timestep = 1 / 60
 const material = new THREE.MeshStandardMaterial({
 	map: loader.load('textureOne.jpg') //test texture
 });
-const TargetArr=[];
-const mapTargetArr=[];
+const TargetArr = [];
+const mapTargetArr = [];
 const world = new CANNON.World({
 	gravity: new CANNON.Vec3(0, -22, 0) //Middle value is gravity in the y direction 
 });
 
-const planeMaterial =new CANNON.Material({
+const planeMaterial = new CANNON.Material({
 	friction: 10,
 	restitution: 0
 })
 
-var hud=new HUD(20,30,5,0);
+var hud = new HUD(20, 30, 5, 0);
 
-var hudTexture=new THREE.Texture(hud.getCanvas())
+var hudTexture = new THREE.Texture(hud.getCanvas())
 
 //hudTexture.repeat.set((window.innerWidth-20)/)
-hudTexture.needsUpdate=true;
-var hudMat=new THREE.MeshBasicMaterial({map:hudTexture});
-hudMat.transparent=true
-console.log(window.innerWidth/hudTexture.image.width, window.innerHeight/hudTexture.image.height)
-var HudGeom=new THREE.BoxGeometry(window.innerWidth,window.innerHeight,0)
-var HudPlane=new THREE.Mesh(HudGeom,hudMat)
-HudPlane.material.depthTest=false;
-HudPlane.material.depthWrite=false;
-HudPlane.onBeforeRender=function(renderer){
+hudTexture.needsUpdate = true;
+var hudMat = new THREE.MeshBasicMaterial({ map: hudTexture });
+hudMat.transparent = true
+console.log(window.innerWidth / hudTexture.image.width, window.innerHeight / hudTexture.image.height)
+var HudGeom = new THREE.BoxGeometry(window.innerWidth, window.innerHeight, 0)
+var HudPlane = new THREE.Mesh(HudGeom, hudMat)
+HudPlane.material.depthTest = false;
+HudPlane.material.depthWrite = false;
+HudPlane.onBeforeRender = function (renderer) {
 	renderer.clearDepth();
 }
 sceneHUD.add(HudPlane)
 
-
-for(var i=0;i<5;i++){
-	var target=new Targets(i,2+3*i,5,2*i);
-	TargetArr.push(target)
-	scene.add(target.getCylinder())
-	
-	
-}
-var target=new Targets(5,0,15,0);
-TargetArr.push(target)
-scene.add(target.getCylinder())
-
-hud.updateTargetNumbers(TargetArr.length,0)
+addTargets([[8, 3, 5], [10, 6, 2], [3, 3, 3]]);
+console.log(TargetArr.length)
+hud.updateTargetNumbers(TargetArr.length, 0)
 
 
 //Import the level from Blender and apply physics bounding
@@ -87,18 +77,18 @@ const models = {
 	for (const model of Object.values(models)) {
 		gltfLoader.load(model.url, (gltf) => {
 			const root = gltf.scene;
-			root.name="house";
+			root.name = "house";
 			const obj3D = root.getObjectByName('Base')
 			scene.add(root);
 			//TODO FIX THIS BROKEN CODE BELOW -----------------------------------------------------------
 			//Convert root to cannon object
 			const result = threeToCannon(obj3D);
-			const {shapeLevel1, offsetLevel1, quaternionLevel1} = result;
+			const { shapeLevel1, offsetLevel1, quaternionLevel1 } = result;
 			//console.log(result.offset)
 
-			const level1CollisionBody=new CANNON.Body()
+			const level1CollisionBody = new CANNON.Body()
 			result.offset
-			level1CollisionBody.addShape(result.shape,result.offset, result.quaternion)
+			level1CollisionBody.addShape(result.shape, result.offset, result.quaternion)
 			world.addBody(level1CollisionBody);
 			//console.log("added House")
 			//-------------------------------------------------------------------------------------------
@@ -117,13 +107,13 @@ const models = {
 }
 
 
-const geometry = new THREE.BoxGeometry(1,1,1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 const floorGeo = new THREE.BoxGeometry(100, 0.1, 100);
-const floorMat = new THREE.MeshLambertMaterial({map: loader.load("goomba.png")}); //testure on floor to show depth of movement
+const floorMat = new THREE.MeshLambertMaterial({ map: loader.load("goomba.png") }); //testure on floor to show depth of movement
 
 const light = new THREE.HemisphereLight("white", "white", 0.5);
 const floor = new THREE.Mesh(floorGeo, floorMat);
-floor.receiveShadow=true;
+floor.receiveShadow = true;
 
 
 
@@ -132,10 +122,10 @@ floor.receiveShadow=true;
 
 
 const groundBody = new CANNON.Body({
-	shape: new CANNON.Box(new CANNON.Vec3(100,0.1,100)), //have floor be really thin box since plane was having collision issues 
+	shape: new CANNON.Box(new CANNON.Vec3(100, 0.1, 100)), //have floor be really thin box since plane was having collision issues 
 	mass: 0, //no mass so it does not fall
 	type: CANNON.Body.STATIC, //does not move
-	material:planeMaterial //to add friction 
+	material: planeMaterial //to add friction 
 });
 groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 0), Math.PI * 0.5); //flat surface 
 world.addBody(groundBody) //add floor to world
@@ -143,54 +133,54 @@ scene.add(light)
 scene.add(floor.translateZ(-6).translateY(-2))
 camera.position.z = 9;
 camera.position.y = 9;
-pipcamera.position.set( 0,30, 0);
-pipcamera.rotateX(-Math.PI/2)
+pipcamera.position.set(0, 30, 0);
+pipcamera.rotateX(-Math.PI / 2)
 
-
-
+const initcam=camera.quaternion
+console.log(initcam)
 
 const player = new THREE.Mesh(new THREE.SphereGeometry(1.5), material);  //visibile representation of player hitbox
-player.castShadow=true;
-player.receiveShadow=true;
+player.castShadow = true;
+player.receiveShadow = true;
 scene.add(player)
 const playerShape = new CANNON.Sphere(1.5);
 const playerBody = new CANNON.Body({ //player hitbox represented by sphere 
 	mass: 1,
 	shape: playerShape,
-	position: new CANNON.Vec3(0, 5, 4),
+	position: initposition,
 	quaternion: new THREE.Quaternion(),
-	material:planeMaterial //to add friction 
+	material: planeMaterial //to add friction 
 });
 
-playerBody.pitchObject=new THREE.Object3D()
+playerBody.pitchObject = new THREE.Object3D()
 playerBody.pitchObject.add(camera)
-playerBody.noBullets=20;
-playerBody.yawObject=new THREE.Object3D()
-playerBody.yawObject.position.z=5;
-playerBody.yawObject.position.y=2;
+playerBody.noBullets = 20;
+playerBody.yawObject = new THREE.Object3D()
+playerBody.yawObject.position.z = 5;
+playerBody.yawObject.position.y = 2;
 playerBody.yawObject.add(playerBody.pitchObject)
-playerBody.euler=new THREE.Euler()
-playerBody.canJump=false;
+playerBody.euler = new THREE.Euler()
+playerBody.canJump = false;
 
-const contNorm=new CANNON.Vec3()
-const upAxis=new CANNON.Vec3(0,1,0);
-playerBody.addEventListener('collide', (event)=>{
-	const {contact}=event
-	if(contact.bi.id== playerBody.id){
+const contNorm = new CANNON.Vec3()
+const upAxis = new CANNON.Vec3(0, 1, 0);
+playerBody.addEventListener('collide', (event) => {
+	const { contact } = event
+	if (contact.bi.id == playerBody.id) {
 		contact.ni.negate(contNorm)
-	}else{
+	} else {
 		contNorm.copy(contact.ni)
 	}
-	if(contNorm.dot(upAxis)>0.5){
-		playerBody.canJump=true
+	if (contNorm.dot(upAxis) > 0.5) {
+		playerBody.canJump = true
 	}
 })
 
 
-const direcLight=new THREE.DirectionalLight(0xffffff, 1);
-direcLight.position.set(0,15,0);
-direcLight.target=player
-direcLight.castShadow=true;
+const direcLight = new THREE.DirectionalLight(0xffffff, 1);
+direcLight.position.set(0, 15, 0);
+direcLight.target = player
+direcLight.castShadow = true;
 scene.add(direcLight)
 
 playerBody.linearDamping = 0.9;
@@ -212,28 +202,36 @@ controls.addEventListener('unlocked', () => {
 	controls.enabled = false;
 })
 
-document.addEventListener("mousedown",(e)=>{
-	if(playerBody.noBullets>0) 
-	{
+document.addEventListener("mousedown", (e) => {
+	if (playerBody.noBullets > 0) {
 		playerBody.noBullets--;
 
-		raycaster.setFromCamera(new THREE.Vector2(0,0),camera);
-		const intersects=raycaster.intersectObjects(scene.children);
-		outer:for(let i=0;i<intersects.length;i++){
-			for (let j=0;j<TargetArr.length;j++){
-				if(intersects[i].object==TargetArr[j].getCylinder() && TargetArr[j].isHit==false){
+		raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
+		const intersects = raycaster.intersectObjects(scene.children);
+		outer: for (let i = 0; i < intersects.length; i++) {
+			for (let j = 0; j < TargetArr.length; j++) {
+				if (intersects[i].object == TargetArr[j].getCylinder() && TargetArr[j].isHit == false) {
 					HitTarget(intersects[i].object.name)
 					hud.increaseTarget();
 					break outer;
 				}
 			}
 		}
-		renderer.readRenderTargetPixels(scene,camera)
+		renderer.readRenderTargetPixels(scene, camera)
+		if(playerBody.noBullets==0){
+			removeTargets();
+		}
 	}
-	else if (hud.gamestate==-1) // game fail
+	if (hud.gamestate == -1 ) // game fail
 	{
-
+	init();
 		
+
+	}
+	else if (hud.gamestate==1){ //game win (only one level so just resets)
+		removeTargets();
+		
+		init();
 	}
 })
 
@@ -247,97 +245,129 @@ document.addEventListener("keyup", (e) => {
 	pressedKeys[e.key] = false;
 });
 
-function HitTarget(name){
+function HitTarget(name) {
 	TargetArr[parseInt(name)].hit();
 }
 
 function move() {
-	
-	playerBody.pitchObject.rotation.x=Math.max(-Math.PI / 2, Math.min(Math.PI / 2,camera.rotation.x))
-	
-	playerBody.yawObject.rotation.y=camera.rotation.y;
 
-	var tempVec=new THREE.Vector3(0,0,0);
-	var delta=Clock.getDelta()*1000
+	playerBody.pitchObject.rotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, camera.rotation.x))
+
+	playerBody.yawObject.rotation.y = camera.rotation.y;
+
+	var tempVec = new THREE.Vector3(0, 0, 0);
+	var delta = Clock.getDelta() * 1000
 	if (controls.isLocked) {
-	
+
 		if (pressedKeys['w']) {
-			tempVec.z=-0.05*delta
+			tempVec.z = -0.05 * delta
 		}
 		if (pressedKeys['a']) {
-			tempVec.x=-0.05*delta
+			tempVec.x = -0.05 * delta
 		}
 		if (pressedKeys["d"]) {
-			tempVec.x=0.05*delta
+			tempVec.x = 0.05 * delta
 		}
 		if (pressedKeys['s']) {
-			tempVec.z=0.05*delta
+			tempVec.z = 0.05 * delta
 		}
-		if (pressedKeys[" "] ) {
-			if( playerBody.canJump==true){
-				playerBody.velocity.y=20
+		if (pressedKeys[" "]) {
+			if (playerBody.canJump == true) {
+				playerBody.velocity.y = 20
 			}
-			playerBody.canJump=false
+			playerBody.canJump = false
 		}
-	
+
 	}
 
 	playerBody.quaternion.copy(camera.quaternion)
 	tempVec.applyQuaternion(playerBody.quaternion);
-	playerBody.velocity.x+=tempVec.x
-	playerBody.velocity.z+=tempVec.z
+	playerBody.velocity.x += tempVec.x
+	playerBody.velocity.z += tempVec.z
 	playerBody.yawObject.position.copy(playerBody.position)
 	camera.position.copy(playerBody.position);
-	pipcamera.position.x=(playerBody.position.x);
-	pipcamera.position.z=(playerBody.position.z);
-	
+	pipcamera.position.x = (playerBody.position.x);
+	pipcamera.position.z = (playerBody.position.z);
+
 }
 
 floor.position.copy(groundBody.position);
 floor.quaternion.copy(groundBody.quaternion);
 function animate() {
 	world.step(timestep);
-	
-	
+
+
 	player.position.copy(playerBody.position);
 	player.quaternion.copy(playerBody.quaternion);
 	requestAnimationFrame(animate);
 
 	move();
 
-	hud.updateAmmoCount(playerBody.noBullets,30)
+	hud.updateAmmoCount(playerBody.noBullets, 30)
 	hud.draw();
-	hudTexture.needsUpdate=true;
-	renderer.autoClear=false;
+	hudTexture.needsUpdate = true;
+	renderer.autoClear = false;
 	renderer.clear();
-	renderer.setViewport( 0, 0, window.innerWidth-20, window.innerHeight-20 );
+	renderer.setViewport(0, 0, window.innerWidth - 20, window.innerHeight - 20);
 	renderer.render(scene, camera)
-	renderer.render(sceneHUD,HudCamera)
-    mapTargets();
+	renderer.render(sceneHUD, HudCamera)
+	mapTargets();
 	renderer.clearDepth();
-	renderer.setViewport( window.innerWidth-250,  50, 200, 200 )
+	renderer.setViewport(window.innerWidth - 250, 50, 200, 200)
 	renderer.render(scene, pipcamera);
 	worldTargets();
-	
-	
+
+
 };
 
 animate();
 
 
-function mapTargets(){
-	for (var i=0;i<TargetArr.length;i++){
+function mapTargets() {
+	for (var i = 0; i < TargetArr.length; i++) {
 		//scene.remove(TargetArr[i].getCylinder())
-		var tempCylinder=new THREE.Mesh(TargetArr[i].getCylinder().geometry,TargetArr[i].getCylinder().material)
+		var tempCylinder = new THREE.Mesh(TargetArr[i].getCylinder().geometry, TargetArr[i].getCylinder().material)
 		tempCylinder.position.copy(TargetArr[i].getCylinder().position)
 		mapTargetArr.push(tempCylinder)
-		scene.add(tempCylinder.rotateY(Math.PI/2).translateY(5))
+		scene.add(tempCylinder.rotateY(Math.PI / 2).translateY(5))
 	}
 
+
+}
+function worldTargets() {
+	while (mapTargetArr.length != 0) {
+		scene.remove(mapTargetArr.pop())
+	}
+}
+
+
+function addTargets(position) {
+
+	for (var i = 0; i < position.length; i++) {
+		var target = new Targets(i, position[i][0], position[i][1], position[i][2]);
+		TargetArr.push(target)
+		scene.add(target.getCylinder())
+
+
+	}
+
+}
+function init() {
+
+	addTargets([[8, 3, 5], [10, 6, 2], [3, 3, 3]]);
+	hud.gamestate = 0;
+	hud.currtargets = 0;
+	playerBody.noBullets=20;
+	hud.updateAmmoCount(playerBody.noBullets, 30);
+	playerBody.velocity=new CANNON.Vec3(0,0,0)
+	playerBody.position.copy(initposition)
+	camera.position.copy(playerBody.position)
+	camera.lookAt(0,0,0)
+	playerBody.quaternion.copy(camera.quaternion)
 	
 }
-function worldTargets(){
-	while(mapTargetArr.length!=0){
-		scene.remove(mapTargetArr.pop())
+function removeTargets() {
+	while (TargetArr.length != 0) {
+		scene.remove(TargetArr.pop().getCylinder())
 	}
 }
