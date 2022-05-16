baf50647961b34c485a61abfa0aadfb5f31f2a65
2333263 <2333263@students.wits.ac.za>
2022-05-16 17:07:26 +0200

    another test

--- js/targets.js
+++ js/targets.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import * as CANNON from '../node_modules/cannon-es/dist/cannon-es.js';
 
 const loader = new THREE.TextureLoader();
