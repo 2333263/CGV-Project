632f8a710562d0e366dec1e201dae10ce4107704
2333263 <2333263@students.wits.ac.za>
2022-06-05 18:25:38 +0200

    banana gun now gets set properly

--- js/BuildWorld.js
+++ js/BuildWorld.js
@@ -658,6 +658,21 @@ class BuildWorld {
 
 
         gltfLoader.load(url, (gltf) => {
+            //for some unknown reason this code needs to be repeated here
+            if((scene.getObjectByName('handRight').getObjectByName('weaponsM4') &&banana)|| (scene.getObjectByName('handRight').getObjectByName('muzzleFlash')&&banana)){
+                var children=scene.getObjectByName('handRight').children
+                for(var i=0;i<children.length;i++){
+                    if(children[i].name=="weaponsM4"){
+                        scene.getObjectByName('handRight').remove(children[i])
+                    }else if(children[i].name=="muzzleFlash"){
+                        scene.getObjectByName('handRight').remove(children[i])
+                    }
+                }
+            }
+
+
+
+
             const weapon = gltf.scene
             if(!banana){
             weapon.name = 'weaponsM4'
