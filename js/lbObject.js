ae06fe628b0b25186bc63c1269f18862e79cca45
jeremyscodes <2368841@students.wits.ac.za>
2022-06-08 16:26:58 +0200

    js doc Entry comments

--- js/lbObject.js
+++ js/lbObject.js
@@ -1,4 +1,12 @@
+/**
+ * @classdesc Entry used to create a pair: An entry in the leaderboard
+ */
 class Entry{
+    /**
+     * @constructor Entry Constructor
+     * @param {string} Name
+     * @param {float} Time 
+     */
 constructor(Name,Time){
     this.name=Name;
     this.time=Time;
